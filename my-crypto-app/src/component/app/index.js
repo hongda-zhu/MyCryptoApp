9c3dd80310872d843b30e7dde353ef7c5a742fbc
Zhu <41998360+AdriaanZhu@users.noreply.github.com>
2020-01-30 12:07:27 +0100

    add styled-component

--- my-crypto-app/src/component/app/index.js
+++ my-crypto-app/src/component/app/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react'
 import './App.css';
+// import { render } from 'react-dom';
+// import { func } from 'prop-types';
+import WelcomeMessage from '../WelcomeMessage'
+import styled, {css} from 'styled-components'
 
-function App() {
-  return (
-    <div>Hello World</div>
-  );
+const MyButton = styled.button`
+  color: green;
+  ${
+    props => props.primary && css`
+      color: palevioletred;
+    `}
+`
+
+const TomatoButton = styled(MyButton)`
+    color: tomato;
+    border-color: tomato;
+`
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+
+        <WelcomeMessage />
+        <MyButton>Hello </MyButton>
+        <MyButton primary> Hello</MyButton>
+        <TomatoButton primary> Hello</TomatoButton>
+
+      </div>
+
+    )
+  }
+
 }
 
 export default App;
