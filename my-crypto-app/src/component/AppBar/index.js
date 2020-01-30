62583e56bc7e0672a0498127262e3ba2d235a85d
Zhu <41998360+AdriaanZhu@users.noreply.github.com>
2020-01-30 12:26:43 +0100

    welcomeMessage improved

--- my-crypto-app/src/component/AppBar/index.js
+++ my-crypto-app/src/component/AppBar/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Bar = styled.div`
-    display:grid
-    grid-template-columns: 180px 1fr 100px
+    display:grid;
+    grid-template-columns: 1.8fr 2fr 1fr 1fr;
 `
 
 export default function(){
     return <Bar> 
 
         <div> child 1 </div>
+        <div/>
         <div> child 2 </div>
         <div> child 3 </div>
         
