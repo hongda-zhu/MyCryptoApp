f068d9854874871d4bf10ff3d4cf1a5ebffbbc4a
Zhu <41998360+AdriaanZhu@users.noreply.github.com>
2020-01-31 21:45:45 +0100

    style add bar

--- my-crypto-app/src/component/AppBar/index.js
+++ my-crypto-app/src/component/AppBar/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const Logo = styled.div`
+    font-size: 1.5em
+`
 
 const Bar = styled.div`
     display:grid;
     grid-template-columns: 1.8fr 2fr 1fr 1fr;
 `
 
+const ControlButtonElem = styled.div`
+    cursor: pointer;
+    ${props => props.active && css `
+        text-shadow: 0px 0px 60px #03ff03;
+        margin-left: 1rem
+    `}
+`
+function ControButton({name, active}){
+    return (
+        <ControlButtonElem active={active}> 
+            {name}
+        </ControlButtonElem>
+    )
+}
+
 export default function(){
     return <Bar> 
 
-        <div> child 1 </div>
+        <Logo> MyCryptoApp </Logo>
         <div/>
-        <div> child 2 </div>
-        <div> child 3 </div>
+        <ControButton name="Dashboard" active></ControButton>
+        <ControButton name="Settings" active></ControButton>
         
     </Bar>
 }
